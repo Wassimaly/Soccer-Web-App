3f3c75da3388848f192d17b7ef8ff64351fddcc8
Wassimaly <78573453+Wassimaly@users.noreply.github.com>
2021-10-27 11:51:26 -0500

    Add files via upload

---
+++ Models/Soccer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerWebApp.Models
+{
+    public class Soccer
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public string Manager { get; set; }
+        public bool ChampionsLeague { get; set; }
+        
+
+        public Soccer()
+        {
+
+        }
+    }
+}
